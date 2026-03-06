df9ba2c42c4dcb403faa96dbdd3fb3827fb5848f
Joana Cullhaj <joanacullhaj@gmail.com>
2026-03-06 18:21:34 +0100

    fist changes

--- App.tsx
+++ App.tsx
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let messageObj = {
+    name : 'joana',
+    age : 33
+  };
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! { messageObj.name}</Text>
       <StatusBar style="auto" />
     </View>
   );
